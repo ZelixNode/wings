5464330feff13815d51ec92934bfd755dbf7da91
maikel Middelbos <53620759+smikkelmikkel@users.noreply.github.com>
2022-07-16 13:02:50 +0200

    Update wings.bash

--- wings.bash
+++ wings.bash
@@ -1,6 +1,7 @@
 echo "[Flatnode] Installatie is gestard!"
 sleep 5s
 apt install docker 
+curl -sSL https://get.docker.com/ | CHANNEL=stable bash
 systemctl enable --now docker
 mkdir -p /etc/pterodactyl
 curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
