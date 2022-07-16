8f502e2d253bcd6c8b96dcdb20b391cb7110bcb3
maikel Middelbos <53620759+smikkelmikkel@users.noreply.github.com>
2022-07-16 13:01:52 +0200

    Update wings.bash

--- wings.bash
+++ wings.bash
@@ -5,6 +5,8 @@ systemctl enable --now docker
 mkdir -p /etc/pterodactyl
 curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
 chmod u+x /usr/local/bin/wings
+curl -o /etc/systemd/system/wings.service https://raw.githubusercontent.com/ZelixNode/wings/main/wings.service
+
 
 echo "ssl domein"
 
