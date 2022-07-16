71e986ed15cf816c3a7de74d801e1ce46c76a5d8
maikel Middelbos <53620759+smikkelmikkel@users.noreply.github.com>
2022-07-16 13:00:49 +0200

    Create wings.bash

---
+++ wings.bash
@@ -0,0 +1,18 @@
+echo "[Flatnode] Installatie is gestard!"
+sleep 5s
+apt install docker 
+systemctl enable --now docker
+mkdir -p /etc/pterodactyl
+curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+chmod u+x /usr/local/bin/wings
+
+echo "ssl domein"
+
+read ssldomein
+
+apt install nginx -y
+sudo apt update
+sudo apt install -y certbot
+sudo apt install -y python3-certbot-nginx
+
+certbot certonly --nginx -d $ssldomein
